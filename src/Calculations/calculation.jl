34fbc2b5cc5f082bd16a69126a2ce9ef875bd7d4
Guoyuan Liu <liuguoyuan01@outlook.com>
2023-12-19 17:41:26 +0100

    fix documentation

--- src/Calculations/calculation.jl
+++ src/Calculations/calculation.jl
@@ -2,7 +2,7 @@
     InputData(name::Symbol, option::Symbol, data::Any)
 
 Represents a more structured block of input data.
-e.g. `InputData(:k_points, :automatic, (6,6,6,1,1,1))`
+e.g. `InputData(:k_points, :automatic, [6,6,6,1,1,1])`
 would be translated for a QE calculation into
 ```
 K_POINTS(automatic)
